4b99f35db49d2f3872386967fe12042904e2a085
Paul Podolin <q12w@bk.ru>
2020-09-08 09:09:14 +0300

    Create python37.sh

---
+++ python37.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev curl libbz2-dev
+curl -O https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
+tar -xf Python-3.7.3.tar.xz
+cd Python-3.7.3
+./configure --enable-optimizations
+make -j 4
+sudo make altinstall
+python3.7 --version
